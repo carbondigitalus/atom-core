1b876cb367914fccb3dac85de6691acfc79977da
Jared Ledbetter <jared@carbondigital.us>
2025-08-24 13:16:45 -0400

    setup jsxDEV runtime

---
+++ src/runtime/jsx-dev-runtime.ts
@@ -0,0 +1,48 @@
+// Custom Modules
+
+import { createElement } from '../core/createElement';
+import { VNode } from '../utils/interfaces/VNode';
+import { Children } from '../utils/types/Children';
+import { ElementType } from '../utils/types/ElementType';
+import { Key } from '../utils/types/Key';
+import { Props } from '../utils/types/Props';
+import { Source } from '../utils/types/Source';
+export { Fragment } from './jsx-runtime';
+
+export function jsxDEV<P = Props>(
+  type: ElementType<P>,
+  props?: P & Props,
+  key?: Key,
+  _isStaticChildren?: boolean,
+  source?: Source,
+  self?: unknown
+): VNode<P> {
+  const src = (props ?? {}) as Props;
+  const children = (src as { children?: Children }).children;
+
+  const baseWithDev = {
+    ...src,
+    ...(key === undefined ? {} : { key }),
+    __source: source,
+    __self: self
+  } as unknown as P & Props;
+
+  if (children === undefined) {
+    return createElement<P>(type, baseWithDev);
+  }
+
+  // Remove children from props to avoid passing it twice
+  const {
+    children: _dropChildren, // rename so we can omit it
+    ...withoutChildren
+  } = baseWithDev as unknown as Props & { children?: Children };
+  void _dropChildren; // satisfy no-unused-vars
+
+  return Array.isArray(children)
+    ? createElement<P>(
+        type,
+        withoutChildren as unknown as P & Props,
+        ...children
+      )
+    : createElement<P>(type, withoutChildren as unknown as P & Props, children);
+}
