eafa4c233f28f75d0108f8edf5713c7a67656c54
Jared Ledbetter <jared@carbondigital.us>
2025-08-24 13:17:02 -0400

    setup VNode interface

---
+++ src/utils/interfaces/VNode.ts
@@ -0,0 +1,9 @@
+// Custom Modules
+import { Children } from '../types/Children';
+import { ElementType } from '../types/ElementType';
+import { Key } from '../types/Key';
+
+export interface VNode<P = any> {
+  readonly type: ElementType<P>;
+  readonly props: P & { key?: Key; children?: Children };
+}
