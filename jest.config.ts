b9a583147ddc45fde78e482db1eb50cba58e92cd
Jared Ledbetter <jared@carbondigital.us>
2025-08-28 06:28:18 -0400

    Create jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+// NPM Modules
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  reporters: [
+    'default',
+    [
+      'jest-allure2-reporter',
+      {
+        resultsDir: 'reports-jest/data'
+      }
+    ]
+  ]
+};
+
+export default config;
