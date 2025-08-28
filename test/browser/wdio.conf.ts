cc571192e40a60b63c9532a18fa12d205fa96a33
Jared Ledbetter <jared@carbondigital.us>
2025-08-28 19:36:40 -0400

    Create wdio.conf.ts

---
+++ test/browser/wdio.conf.ts
@@ -0,0 +1,49 @@
+// NPM Modules
+import type { Options } from '@wdio/types';
+import 'dotenv/config';
+
+export const config = {
+  autoCompileOpts: {
+    tsNodeOpts: { transpileOnly: true, project: './browser/tsconfig.json' }
+  },
+  baseUrl: 'http://localhost:8080',
+  capabilities: [
+    {
+      browserName: 'chrome',
+      'goog:chromeOptions': {
+        args: ['--headless=new', '--disable-gpu', '--window-size=1400,1000']
+      }
+    }
+  ],
+  connectionRetryCount: 2,
+  connectionRetryTimeout: 90_000,
+  framework: 'mocha',
+  logLevel: 'info',
+  maxInstances: 1,
+  mochaOpts: { ui: 'bdd', timeout: 60_000 },
+  reporters: [
+    'spec',
+    [
+      'allure',
+      {
+        outputDir: 'reports/allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: false
+      }
+    ]
+  ],
+  runner: 'local',
+  services: [
+    [
+      'static-server',
+      {
+        folders: [{ mount: '/', path: './test/browser/fixtures' }],
+        port: 8080,
+        middleware: []
+      }
+    ],
+    'devtools'
+  ],
+  specs: ['./specs/**/*.spec.ts'],
+  waitforTimeout: 10_000
+} as unknown as Options.Testrunner;
