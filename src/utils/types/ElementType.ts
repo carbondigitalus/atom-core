24de5936d97a8580425b386dd23846fe631b2b7d
Jared Ledbetter <jared@carbondigital.us>
2025-08-29 02:47:19 -0400

    update type for new component structure

--- src/utils/types/ElementType.ts
+++ src/utils/types/ElementType.ts
@@ -1,4 +1,4 @@
 // Custom Modules
 import { Component } from './Component';
 
-export type ElementType<P = any> = string | Component<P>;
+export type ElementType<P = any> = string | (new (props: P) => Component<P>);
