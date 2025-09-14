935f5fcf3683195471abdeac04acb59b0a2451e6
Jared Ledbetter <jared@carbondigital.us>
2025-09-14 03:11:42 -0400

    Update BeforeMountCapableComponent.ts

--- src/utils/interfaces/BeforeMountCapableComponent.ts
+++ src/utils/interfaces/BeforeMountCapableComponent.ts
@@ -3,7 +3,7 @@
  * Handles the beforeMount phase of component mounting
  */
 
-export interface BeforeMountCapableComponent {
+export default interface BeforeMountCapableComponent {
   __enterMountPhase?: () => void;
   __exitMountPhase?: () => void;
   __canInvokeBeforeMount?: () => boolean;
