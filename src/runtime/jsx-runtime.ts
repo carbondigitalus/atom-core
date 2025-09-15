65ca57208e7f1d6813f26a13065e3c1bd34b37c8
Jared Ledbetter <jared@carbondigital.us>
2025-09-15 02:15:49 -0400

    Update jsx-runtime.ts

--- src/runtime/jsx-runtime.ts
+++ src/runtime/jsx-runtime.ts
@@ -7,20 +7,13 @@ import { ElementType } from '../utils/types/ElementType';
 import { Key } from '../utils/types/Key';
 import { Props } from '../utils/types/Props';
 
-export const Fragment = (p: { children?: Children }): Children | null =>
-  p.children ?? null;
+export const Fragment = (p: { children?: Children }): Children | null => p.children ?? null;
 
-export function jsx<P = Props>(
-  type: ElementType<P>,
-  props?: P & Props,
-  key?: Key
-): VNode<P> {
+export function jsx<P = Props>(type: ElementType<P>, props?: P & Props, key?: Key): VNode<P> {
   const src = (props ?? {}) as Props;
   const children = (src as { children?: Children }).children;
 
-  const rest = Object.fromEntries(
-    Object.entries(src).filter(([k]) => k !== 'children')
-  ) as P & Props;
+  const rest = Object.fromEntries(Object.entries(src).filter(([k]) => k !== 'children')) as P & Props;
 
   const nextProps = key === undefined ? rest : ({ ...rest, key } as P & Props);
 
@@ -33,18 +26,12 @@ export function jsx<P = Props>(
   return createElement<P>(type, nextProps, ...flat);
 }
 
-export function jsxs<P = Props>(
-  type: ElementType<P>,
-  props?: P & Props,
-  key?: Key
-): VNode<P> {
+export function jsxs<P = Props>(type: ElementType<P>, props?: P & Props, key?: Key): VNode<P> {
   const src = (props ?? {}) as Props;
   const children = (src as { children?: Children }).children;
 
   // Build props without `children`
-  const rest = Object.fromEntries(
-    Object.entries(src).filter(([k]) => k !== 'children')
-  ) as P & Props;
+  const rest = Object.fromEntries(Object.entries(src).filter(([k]) => k !== 'children')) as P & Props;
 
   const nextProps = key === undefined ? rest : ({ ...rest, key } as P & Props);
 
