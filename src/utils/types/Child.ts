80be56b58c992cda916c38a2873ade98526a210d
Jared Ledbetter <jared@carbondigital.us>
2025-09-14 23:20:37 -0400

    Update Child.ts

--- src/utils/types/Child.ts
+++ src/utils/types/Child.ts
@@ -1,5 +1,5 @@
 // Custom Modules
-import { VNode } from '../interfaces/VNode';
+import VNode from '../interfaces/VNode';
 import { PrimitiveChild } from './PrimitiveChild';
 
 export type Child = VNode<any> | PrimitiveChild | boolean | null | undefined;
