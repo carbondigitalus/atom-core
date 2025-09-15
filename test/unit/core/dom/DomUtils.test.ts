f3f675208d72b19b2fa664bc254e80c6fc67381b
Jared Ledbetter <jared@carbondigital.us>
2025-09-14 23:20:42 -0400

    Create DomUtils.test.ts

---
+++ test/unit/core/dom/DomUtils.test.ts
@@ -0,0 +1,10 @@
+/** @jest-environment jsdom */
+
+// NPM Modules
+import { expect } from '@jest/globals';
+
+describe('Placeholder', () => {
+  it('should have tests', () => {
+    expect(true).toBe(true);
+  });
+});
